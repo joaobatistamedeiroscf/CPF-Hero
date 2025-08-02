c1ef1a86eab05a37c41bbaa5c8ab06f91f68e00c
joaobatistamedeiroscf <joaobatistamedeiroscf@gmail.com>
2025-08-02 19:04:56 -0300

    adicionando funcionalidade copiar e arquitetura de pastas

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,19 @@
-import { validaCpf } from './validaCpf.js';
-import { geraCpf } from './geraCpf.js';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { validaCpf } from './utils/validaCpf.js';
+import { geraCpf } from './utils/geraCpf.js';
 const botaoVerificarCpf = document.getElementById("botao__verificar");
 const inputCpf = document.getElementById("cpf");
 const mensagem = document.getElementById("mensagem");
 const botaoGerarCpf = document.getElementById("gerar__cpf");
+const botaoCopiaCpf = document.getElementById("copy-cpf");
 botaoVerificarCpf.addEventListener("click", (evento) => {
     evento.preventDefault();
     const cpf = inputCpf.value;
@@ -28,4 +38,23 @@ botaoGerarCpf.addEventListener("click", (evento) => {
     mensagem.textContent = "✅🙌 CPF gerado com sucesso !";
     mensagem.className = "mensagem sucesso";
     mensagem.style.display = "block";
+    setTimeout(() => {
+        mensagem.style.display = "none";
+    }, 4000);
 });
+botaoCopiaCpf.addEventListener("click", (evento) => __awaiter(void 0, void 0, void 0, function* () {
+    evento.preventDefault();
+    const cpf = inputCpf.value;
+    if (cpf == "") {
+        alert("⚠️ Nenhum CPF para copiar!");
+        return;
+    }
+    try {
+        yield navigator.clipboard.writeText(cpf);
+        alert(`✅ CPF copiado com sucesso! 🙌: ${cpf}`);
+    }
+    catch (err) {
+        alert("❌ Erro ao copiar CPF. Tente novamente.");
+        console.error("Erro ao copiar para clipboard:", err);
+    }
+}));
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { validaCpf } from './validaCpf.js'; 
-import { geraCpf } from './geraCpf.js';
+import { validaCpf } from './utils/validaCpf.js'; 
+import { geraCpf } from './utils/geraCpf.js';
 
 const botaoVerificarCpf = document.getElementById("botao__verificar") as HTMLButtonElement;
 const inputCpf = document.getElementById("cpf") as HTMLInputElement;
 const mensagem = document.getElementById("mensagem") as HTMLDivElement;
 const botaoGerarCpf = document.getElementById("gerar__cpf") as HTMLButtonElement ; 
+const botaoCopiaCpf =  document.getElementById("copy-cpf") as HTMLButtonElement ; 
 
 botaoVerificarCpf.addEventListener("click", (evento) => {
     evento.preventDefault();
@@ -32,4 +33,26 @@ botaoGerarCpf.addEventListener("click" , (evento)=>{
     mensagem.textContent = "✅🙌 CPF gerado com sucesso !" ; 
     mensagem.className = "mensagem sucesso" ; 
     mensagem.style.display = "block" ; 
+
+     setTimeout(() => {
+        mensagem.style.display = "none";
+}, 4000);
 });
+
+botaoCopiaCpf.addEventListener("click" ,  async (evento)=> {
+    evento.preventDefault();
+    const cpf = inputCpf.value ; 
+    if(cpf == ""){
+        alert("⚠️ Nenhum CPF para copiar!");
+        return ; 
+    }
+    
+    try {
+         await navigator.clipboard.writeText(cpf);
+         alert(`✅ CPF copiado com sucesso! 🙌: ${cpf}`);
+    } catch (err){
+        alert("❌ Erro ao copiar CPF. Tente novamente.");
+        console.error("Erro ao copiar para clipboard:", err);
+    }
+    
+}) ; 
